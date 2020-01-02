18f88fe7fc6f12e1db7818f5c5efe8f6095501cf
Damien Smrt <damien@flipboxdigital.com>
2020-01-02 08:52:56 -0700

    Fixing logging regex

--- @flipboxlabs/ecs-pipeline/lib/ecs-pipeline/ecs/ec2/task-definition.ts
+++ @flipboxlabs/ecs-pipeline/lib/ecs-pipeline/ecs/ec2/task-definition.ts
@@ -66,7 +66,7 @@ export class Ec2TaskDefinitionStack extends SubStack {
   protected memoryReservationMiB: number = 256
   protected devopsBucket: string
   protected multilinePattern: string =
-    '^(\\d\\d\\d\\d-\\d\\d-\\d\\d \\d\\d:\\d\\d:\\d\\d|\\S+:443 \\b\\d{1,3}.\\d{1,3}.\\d{1,3}.\\d{1,3}\\b)'
+    '^(\\d\\d\\d\\d-\\d\\d-\\d\\d \\d\\d:\\d\\d:\\d\\d|\\S+:\\d+ \\b\\d{1,3}.\\d{1,3}.\\d{1,3}.\\d{1,3}\\b|\\d{1,3}.\\d{1,3}.\\d{1,3}.\\d{1,3}\\b)'
 
   constructor(
     scope: cdk.Construct,
